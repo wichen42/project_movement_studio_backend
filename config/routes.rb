c4ad95c53171d79bd880e42c5e7426cbf22f36d4
Wilson Chen <wilsonchen901@gmail.com>
2023-10-17 16:21:15 -0400

    setup rails routes api and v1 namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :posts
+  # API routes should be formatted in /api/v#
+  namespace :api do
+    namespace :v1 do
+      resources :posts
+    end
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

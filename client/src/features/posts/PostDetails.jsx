ecc8f0957be13379656963a6d8bb8125c33eb543
Wilson Chen <wilsonchen901@gmail.com>
2023-10-18 15:54:20 -0400

    implement delete post functionality on index and show page

--- client/src/features/posts/PostDetails.jsx
+++ client/src/features/posts/PostDetails.jsx
@@ -1,5 +1,5 @@
 import {useEffect, useState} from "react"
-import { useParams, Link } from "react-router-dom"
+import { useParams, Link, useNavigate } from "react-router-dom"
 import { API_URL } from "../../constants";
 
 const PostDetails = () => {
@@ -7,6 +7,7 @@ const PostDetails = () => {
     const [, setError] = useState(null);
     const [loading, setLoading] = useState(true);
     const { id } = useParams();
+    const navigate = useNavigate();
 
     useEffect(() => {
         const fetchCurrentPost = async () => {
@@ -30,11 +31,31 @@ const PostDetails = () => {
 
     if (loading) return <h2>Loading....</h2>
 
+    const deletePost = async (id) => {
+        try {
+            const response = await fetch(`${API_URL}/${id}`, {
+                method: "DELETE",
+            });
+
+            if (response.ok) {
+                navigate("/");
+            } else {
+                throw response;
+            }
+        } catch (e) {
+            setError(e)
+            console.log(`Error deleting post ${id}`)
+            console.log("Error: ", e)
+        }
+    };
+
     return (
         <div>
             <h2>{post.title}</h2>
             <p>{post.body}</p>
             <Link to="/">Back to Posts</Link>
+            {" | "}
+            <button onClick={() => deletePost(post.id)}>Delete</button>
         </div>
     )
 }
--- client/src/features/posts/PostList.jsx
+++ client/src/features/posts/PostList.jsx
@@ -29,14 +29,34 @@ const PostList = () => {
     loadPosts();
   }, [])
 
+  const deletePost = async (id) => {
+    try {
+      // DELETE request to: http://localhost:3000/api/v1/posts/:id
+      const response = await fetch(`${API_URL}/${id}`, {
+        method: "DELETE",
+      });
+
+      if (response.ok) {
+        setPosts(posts.filter((post) => post.id !== id));
+      } else {
+        throw response;
+      }
+    } catch (e) {
+      setError(e)
+      console.log(`Error deleting post: ${id}`)
+      console.log("Error: ", e)
+    }
+  };
+
   return (
     <div>
       {posts.map((post) => (
         <div key={post.id} className="post-container">
           <Link to={`/posts/${post.id}`}>{post.title}</Link>
-          <p>{post.body}</p>
           <div>
             <Link to={`/posts/${post.id}/edit`}>Edit</Link>
+            {" | "}
+            <button onClick={() => deletePost(post.id)}>Delete</button>
           </div>
         </div>
       ))}

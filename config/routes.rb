2bd8b8b0822d9dccc5a28ed942846985422d7253
Wilson Chen <wilsonchen901@gmail.com>
2023-10-27 10:59:43 -0400

    create seach_controller and search routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # API routes should be formatted in /api/v#
   namespace :api do
     namespace :v1 do
+      get 'search/posts'
       resources :posts
     end
   end
